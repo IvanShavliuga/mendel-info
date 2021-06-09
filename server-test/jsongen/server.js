4d182e3915312ef68a7f9cb9debb9abebc20087c
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-06-09 19:06:13 +0300

    generate json

--- server-test/jsongen/server.js
+++ server-test/jsongen/server.js
@@ -8,7 +8,6 @@ readFile2("./elements.json", "utf8")
   .then((data) => {
     dt = data.toString();
     jsdt = JSON.parse(data);
-    //console.log(typeof jsdt);
     return data.toString();
   })
   .catch((err) => {
@@ -32,34 +31,34 @@ http
         dtt.lat = "none";
       }
       //let rf = await findPosition('./'+dtt.shortname+'.html')
-      let rf= readFile2('./'+dtt.shortname+'.html', "utf8")
+      let rf= readFile2('./short/'+dtt.shortname+'.html', "utf8")
       .then((data) => {
         let dta = data.toString();
-        let indst = dta.indexOf('<H1') + 1
-        let indend = dta.indexOf('<a name=1')
-        let fnstr = dta.slice(indst, indend)
-        fs.writeFile('./short/'+dtt.shortname+'.html',fnstr,(err) => {
-   if (err) {
-    console.error(err)
-    return
-		}})
+        let indst = dta.indexOf('Положение в таблице')
+        let indend = dta.indexOf('<br>',indst)
+        let ln = 'Положение в таблице'.length
+        let fnstr = dta.slice(indst+ln+5, indend)
+        dtt.pos=fnstr
+        indst = dta.indexOf('Положение в таблице')
+        indend = dta.indexOf('<br>',indst)
+        ln = 'Положение в таблице'.length
+        fnstr = dta.slice(indst+ln+5, indend)
+        dtt.pos=fnstr
         return { findres: fnstr, status: 200}
       })
       .catch((err) => {
         console.log(err);
         return { findres: 'not', status: 404}
       });
+      fs.writeFile('elements-res.json',JSON.stringify(jsdt),(err) => {
+       if (err) {
+        console.error(err)
+        return
+      }})
       res.write(dtt.pos+"");
       res.write(" ");
       console.log(rf)
     }
-  // fs.writeFile('test.txt',JSON.stringify(jsdt), (err) => {
-  // if (err) {
-  //  console.error(err)
-  //  return
-  //}
-  //���� ������� �������
-    // res.write(JSON.stringify(jsdt)); //write a response to the client
-    res.end(); //end the response
+      res.end(); //end the response
   })
   .listen(3000); //the server object listens on port 3000
